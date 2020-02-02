7195cc8c4ebdde6ef0ed0167602ba9a43ff187f1
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-02 23:49:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,6 +49,7 @@ def total_gross(source)
   
   while director_index < source.length do 
     total_gross_of_earnings += gross_for_director( source[director_index])
-    
+    director_index += 1
   end
+  total_gross_of_earnings
 end
