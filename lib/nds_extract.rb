7f057f4ad335729f41c50ee1fca3920b0e8e4988
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-02 23:39:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,4 +43,5 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
 end
